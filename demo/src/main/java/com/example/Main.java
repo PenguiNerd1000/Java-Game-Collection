9f9767821eee9fda9d4695aeaa10b028b26186d7
PenguiNerd1000 <jovanson.zach@gmail.com>
2023-08-23 17:56:16 -0700

    imported 3 classes, added tiles arraylist, started on board, flipped and pairsFound (for the future)

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> tiles = new ArrayList<>();
+        int[] automatic = {1, 2, 3, 4, 5};
+        int x = 0;
+        for (int i = 0; i < 9; i++) {
+            tiles.add(automatic[i]);
+            if (x == 4) {
+                x = 0;
+            }
+            x++;
+        }
+        Collections.shuffle(tiles);
+        String[] board = new String[tiles.size()];
+        boolean[] flipped = new boolean[tiles.size()];
+        int pairsFound = 0;
     }
 }
