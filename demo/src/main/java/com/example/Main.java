b9df4d664d05154dad773a74023c7bf2665f88d1
PenguiNerd1000 <jovanson.zach@gmail.com>
2023-08-30 11:46:25 -0700

    Cleaned up code a little bit, tried fixing automatic function

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,60 +1,45 @@
 package com.example;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.lang.NumberFormatException;
 import java.util.Scanner;
  
 public class Main {
-    public static ArrayList automatic(int a, ArrayList<Integer> tiles) {
-         //shorthand for not writing multiple instances of cards
-        ArrayList<Integer> automatic = new ArrayList<>();
-        int x = 0;
-        for (int i = 0; i < automatic.size()*2; i++) {
-            tiles.add(automatic.get(x));
-            if (x == 4) {
-                x = 0;
-            }            
-            x++;
+    
+    //shorthand for not writing multiple instances of cards
+    public static ArrayList<Integer> automatic(int a, ArrayList<Integer> tiles) {
+         
+         for (int i = 0; i < tiles.size()*2; i++) {
+            if (i < tiles.size()) {
+                tiles.add(i);
+            } else {
+                int counter = i-tiles.size();
+                tiles.add(counter);
+            }
         }
-        return automatic;
+        System.out.println(tiles);
+        return tiles;
     }
-
-    public static int tryStringToInt(String response) {
-        return 1;
-        //return pairsGenerated;
-    }
-
+    
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many pairs do you want to have generated?");       
-        
+        int pairsGenerated = scanner.nextInt();
 
         //tiles used for matching
         ArrayList<Integer> tiles = new ArrayList<>();
-    
-    
-        //automatic(tryStringToInt(response), tiles);
-        int pairsGenerated = 0;
-        String response1 = scanner.nextLine();
-    
-        try {
-            pairsGenerated = Integer.parseInt(response1);
-        } catch (NumberFormatException e) {
-            System.out.println("Please enter a valid integer number");
-        } finally {
-            System.out.println(pairsGenerated);
-        }
-        System.out.println(pairsGenerated);
+        automatic(pairsGenerated, tiles);
+        Collections.shuffle(tiles);
         
-        Collections.shuffle(automatic(pairsGenerated, tiles));
-        //String[] board = new String[tiles.size()];
-        //boolean[] flipped = new boolean[tiles.size()];
-        //int pairsFound = 0; 
-
         //Print out shuffled tiles with for-each loop
         for (int i : tiles) {
             System.out.println(i);
         }
+        
+        //String[] board = new String[tiles.size()];
+        //boolean[] flipped = new boolean[tiles.size()];
+        //int pairsFound = 0; 
+
+        
     }
 }
